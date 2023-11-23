1dc8470cdcb8153831699d162e3e00d78ccde8fe
Pranathitejasvi <151804271+Pranathitejasvi@users.noreply.github.com>
2023-11-23 09:51:38 +0530

    Add files via upload

---
+++ stegnography.py
@@ -0,0 +1,44 @@
+import cv2
+
+import os
+import string
+
+
+img = cv2.imread("flower.jpg")
+msg=input("Enter your secret message")
+password=input("Enter a passcode")
+d={}
+c={}
+for i in range (255):
+    d[chr(i)]=i
+    c[i]=chr(i)
+
+n=0;
+m=0;
+z=0;
+for i in range(len(msg)):
+    img[n,m,z]=d[msg[i]]
+    n=n+1
+    m=m+1
+    z=(z+1)%3
+
+cv2.imwrite("encryptedImage.jpg",img)
+os.startfile("encryptedImage.jpg")
+message =""
+n=0
+m=0
+z=0
+
+pas=input("Enter your password for Descryption")
+if password==pas:
+    for i in range(len(msg)):
+        message=message+c[img[n,m,z]]
+        n=n+1
+        m=m+1
+        z=(z+1)%3
+    print("Decryptedmsg",message)
+else:
+    print("password not valid")
+
+
+
